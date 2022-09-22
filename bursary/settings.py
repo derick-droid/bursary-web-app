01178fcde92858c8a63f992440f1c6e7098a100b
derick-droid <derrickokinda9@gmail.com>
2022-09-22 16:11:53 +0300

    configuration fixing

--- bursary/settings.py
+++ bursary/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static")
-# ] 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
-MEDIA_URL = 'images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+] 
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+# MEDIA_URL = 'images/'
 
 
 # Default primary key field type
