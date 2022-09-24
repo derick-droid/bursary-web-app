a9e411f94c6ce034c8d30d87f07408c01cb318fa
derick-droid <derrickokinda9@gmail.com>
2022-09-24 10:32:54 +0300

    replacing html form file family with django forms

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
-class PersonalForm():
-    name = forms.CharField()
-    gender = forms.ChoiceField(choices=[("male", "Male")("female", "Female") ("other", "Other")], widget=forms.RadioSelect)
+class PersonalForm(forms.Form):
+    name = forms.CharField(required = True,label="Full Names(as it appears id national ID)", max_length=100)
+    # gender = forms.ChoiceField(choices=[("male", "Male")("female", "Female")])
     DATE_OF_BIRTH = forms.TimeField(required = True, label="DATE OF BIRTH")
     ID_NO_passport = forms.IntegerField(required=True, label="ID.NO/PASSPORT")
     
