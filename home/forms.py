181625952f42ae5122dc23ede78cdaf5adc396be
derick-droid <derrickokinda9@gmail.com>
2022-09-24 10:59:59 +0300

    adding radio buttons in the form

--- home/forms.py
+++ home/forms.py
@@ -2,7 +2,11 @@ from django import forms
 
 class PersonalForm(forms.Form):
     name = forms.CharField(required = True,label="Full Names(as it appears id national ID)", max_length=100)
-    # gender = forms.ChoiceField(choices=[("male", "Male")("female", "Female")])
+    gender = forms.ChoiceField(choices="gender", label="Gender")
+    gender = (
+        ("male, Male"),
+        ("female", "Female"),
+        ("other", "Other"),)
     DATE_OF_BIRTH = forms.TimeField(required = True, label="DATE OF BIRTH")
     ID_NO_passport = forms.IntegerField(required=True, label="ID.NO/PASSPORT")
     
